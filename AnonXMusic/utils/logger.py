e2f33693292727c79959326cf6d3863c7d8572ed
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-04-21 11:15:23 +0530

    Update logger.py

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -6,7 +6,7 @@ from config import LOGGER_ID
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        logger_text = f"""
+        logger_text = f"""<blockquote>
 <b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
 <b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
@@ -17,8 +17,8 @@ async def play_logs(message, streamtype):
 <b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
 <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1] if message.text and len(message.text.split(None, 1)) > 1 else "N/A"}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}</blockquote>"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(

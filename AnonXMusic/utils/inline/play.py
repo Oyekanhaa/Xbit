70d45b000c9d598be653a2801fac1bf6d806c61e
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-04-21 11:29:06 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 style=enums.ButtonStyle.PRIMARY,
             )
         ],
-       # [   
+        [   
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
